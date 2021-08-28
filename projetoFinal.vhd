953f773759d39d27343ccd5fa0a2001c765d58ba
Mércia Caroline <merciamartins07@yahoo.com.br>
2021-08-28 13:18:50 -0300

    Lê o arquivo e escreve
    altera estimulos, adiciona debug

--- projetoFinal.vhd
+++ projetoFinal.vhd
@@ -7,52 +7,42 @@ use ieee.numeric_std.all;
 
 entity projetoFinal is
     port (
+		clock: in std_logic;
 		temp_1: in std_logic_vector(15 downto 0);
 		sensor_1: in std_logic;
-		led_1: out std_logic_vector(23 downto 0) -- saida rgb, verificar logica
+		led_1: out std_logic_vector(23 downto 0); -- saida rgb, verificar logica
+		mensagem: out integer
 	 );
 end projetoFinal;
 
 architecture gestao_vacinas of projetoFinal is
 	-- declarações signal
 	signal clk          : std_logic;
-	signal read_vacinas : std_logic := '0';
 	type vect is array (1 to 5) of integer; --O indice do array é o numero do refrigerador 
 	signal vacinas_data: vect;
-	signal mensagem: vect;
-	signal padrao_temp_1 : std_logic_vector(15 downto 0);
-	signal padrao_temp_1_max : std_logic_vector(15 downto 0);
-	signal padrao_temp_1_min : std_logic_vector(15 downto 0);
-	signal start: std_logic;
 	
--- declaraçoes constant
-	constant espaco 	  	: string := " ";
-	constant PERIOD     	: time := 20 ns;
-   constant DUTY_CYCLE 	: real := 0.5;
-   constant OFFSET     	: time := 5 ns; 
-	constant max_value  	: natural := 15;
-	constant mim_value  	:natural := 0;
-	
--- declaracoes variable
-	
-	file vacinas: text open read_mode is "vacina.txt";
-	file relatorio: text open write_mode is "relatorio.txt";
 	
 begin	
 	process(clk)
+		variable padrao_temp_1 : std_logic_vector(15 downto 0);
+		variable padrao_temp_1_max : std_logic_vector(15 downto 0);
+		variable padrao_temp_1_min : std_logic_vector(15 downto 0);
+		
 		begin
 				if (vacinas_data(1) = 1) then
-					padrao_temp_1 <= "1111111110110101";	-- revisar complemento de 2/sinal e magnitude
-					padrao_temp_1_max <= padrao_temp_1;
-					padrao_temp_1_min <= padrao_temp_1;
+					padrao_temp_1 := "1111111110110101";	-- revisar complemento de 2/sinal e magnitude
+					padrao_temp_1_max := padrao_temp_1;
+					padrao_temp_1_min := padrao_temp_1;
 				else
-					padrao_temp_1 <= "0000000000000101";
-					padrao_temp_1_max <= "0000000000001000";
-					padrao_temp_1_min <= "0000000000000010";
+					padrao_temp_1 := "0000000000000101";
+					padrao_temp_1_max := "0000000000001000";
+					padrao_temp_1_min := "0000000000000010";
 				end if;			
+			
 			if ((unsigned(temp_1)) = (unsigned(padrao_temp_1)) AND (sensor_1 = '0')) then		-- temperatura ok, porta fechada
 				led_1 <= x"00ff00";	-- led verde				
-				mensagem(1) <= 1;
+				mensagem <= 1;
+			
 		
 			end if;
 	end process;
--- tb_projetoFinal.vhd
+++ tb_projetoFinal.vhd
@@ -10,30 +10,28 @@ end tb_projetoFinal;
 
 architecture tb_gestao_vacinas of tb_projetoFinal is
 	component vacina port(
-		start: in integer:= 0;
+		clock: in std_logic;
 		temp_1: in std_logic_vector(15 downto 0);
 		sensor_1: in boolean;
-		led_1: out std_logic
+		led_1: out std_logic_vector(23 downto 0);
+		mensagem: out integer
 	);
 	end component;
 	
--- declaraçoes de  signal
-
-	type vect is array (1 to 5) of integer; --O indice do array é o numero do refrigerador 
-	signal vacinas_data: vect;
-	signal mensagem: vect;
-	signal clk: std_logic;
-	signal read_vacinas: std_logic := '0';
-	signal lote_data: string(5 downto 1);
-	signal refrigerador_data: integer;
-	signal flag_write   : std_logic := '0';
-	signal start: std_logic := '0';
-	signal vacinas_output      : std_logic_vector(7 downto 0);
+   -- declaraçoes de  signal ine out
+	type vect is array (1 to 5) of integer; 
+	signal vacinas_data: vect; --O indice do array é o numero do refrigerador  [[[[[[verificar]]]]]]
 	signal porta_1 : boolean;
 	signal temperatura_1 : std_logic_vector(15 downto 0);
-
+	signal alerta_1 : std_logic_vector(23 downto 0);
+	signal notificacao: integer;
 	
-	-- declaraçoes constant
+	--SINAIS DE LEITURA E ESCRITA
+	signal read_data_in    : std_logic:='0';
+   signal flag_write      : std_logic:='0';   
+	
+	-- declaraçoes constant for clock
+	signal clk: std_logic;
 	constant PERIOD     : time := 20 ns;
    constant DUTY_CYCLE : real := 0.5;
    constant OFFSET     : time := 5 ns;
@@ -43,17 +41,24 @@ architecture tb_gestao_vacinas of tb_projetoFinal is
 	-- declaracoes file
 	file vacinas			: text open read_mode is "vacina.txt";
 	file refrigerador_1	: text open write_mode is "refrigerador_1.txt";
-	file refrigerador_2	: text open write_mode is "refrigerador_2.txt";
-	file refrigerador_3	: text open write_mode is "refrigerador_3.txt";
+	
+	--declaracoes de sinal utilizados entre process
+	signal  loteVacina : string(5 downto 1);
+	signal numeroRefrigerador : integer;
 	
 		------------------------------ PORT MAP ---------------------------------------
 begin
 	DUT: vacina
-    port map(temp_1      	=> temperatura_1,
-             sensor_1       => porta_1);
+    port map(clock     => clk,
+				 temp_1    => temperatura_1,
+				 led_1    => alerta_1,
+             sensor_1  => porta_1,
+				 mensagem => notificacao);
 				 
-	-- clock process
-	PROCESS    -- clock process for clock
+------------------------------------------------------------------------------------
+----------------- processo para gerar o sinal de clock 
+------------------------------------------------------------------------------------		
+        PROCESS    -- clock process for clock
         BEGIN
             WAIT for OFFSET;
             CLOCK_LOOP : LOOP
@@ -63,91 +68,78 @@ begin
                 WAIT FOR (PERIOD * DUTY_CYCLE);
             END LOOP CLOCK_LOOP;
         END PROCESS;
-	
+		  
+		  
+------------------------------------------------------------------------------------
+----------------- processo para leer os dados do arquivo vacina.txt
+------------------------------------------------------------------------------------
 
-	--leitura dos dados vacina.txt
 leitura_vacinas_process: process
 	variable linha : line;
 	variable nomeVacina : string(6 downto 1);
 	variable loteVacina : string(5 downto 1);
 	variable numeroRefrigerador : integer;
 	variable espaco 	  : character;
-	variable input : std_logic_vector(3 downto 0);
 	
 	
 	begin
 		wait until(falling_edge(clk));
 		while(not endfile(vacinas)) loop
-			if (start = '1') then
+			if (read_data_in = '1' ) then
 				readline(vacinas, linha);
 				read(linha,nomeVacina);		
-				
 				read(linha, espaco);
-				
 				read(linha,loteVacina);
-				lote_data <= loteVacina;
-				
 				read(linha, espaco);
-				
 				read(linha, numeroRefrigerador);
-				refrigerador_data <= numeroRefrigerador;
-				
+		   report "A vacina lida - " & nomeVacina;
+			report "O lote -" & loteVacina;
+			report "No refrigerador - " & integer'image(numeroRefrigerador);
 				if(nomeVacina = "Pfizer") then
 					vacinas_data(numeroRefrigerador) <= 1; 
 				else 
-					vacinas_data(numeroRefrigerador) <= 2;
-				end if;		
+					vacinas_data(numeroRefrigerador) <= 0;
+				end if;	
+			report "A vacina lida - " & nomeVacina;	
 			end if;
+			wait for PERIOD;
 		end loop;
 		wait;
 	end process leitura_vacinas_process;	
 	
 	
-	
-	-- gera estimulos de entrada
-	gera_estimulos_entrada_process: process
-   begin
-		wait for(OFFSET + 3*PERIOD);
-            start <= '1';		
+------------------------------------------------------------------------------------
+----------------- processo para gerar os estimulos de entrada
+------------------------------------------------------------------------------------
+   tb_stimulus : PROCESS
+   BEGIN
+        WAIT FOR (OFFSET);
+            read_data_in <= '1';		
 			for i in mim_value to max_value loop
-				wait for PERIOD;
-		   end loop;
-            start <= '0';		
-		wait;
-end process gera_estimulos_entrada_process;	
+		        wait for PERIOD;
+		    end loop;
+            read_data_in <= '0';		
+		WAIT;
+   END PROCESS tb_stimulus;	
    	
-   
+------------------------------------------------------------------------------------
+------ processo para gerar os estimulos de escrita do arquivo de saida
+------------------------------------------------------------------------------------ 
+ escreve_outputs : PROCESS
+    BEGIN
+         WAIT FOR (OFFSET + 4*PERIOD);
+             flag_write <= '1';
+			 for i in mim_value to max_value loop
+		         wait for PERIOD;
+		     end loop;
+             flag_write <= '0';			
+		 WAIT;
+    END PROCESS escreve_outputs;   
+	
+-- ------------------------------------------------------------------------------------
+-- ------ processo para escriber os dados de saida no arquivo relatorio.txt
+-- ------------------------------------------------------------------------------------  
 
-	-- gera estimulos de saida
-gera_estimulos_saida_process: process
-	begin
-		wait for(OFFSET + 4*PERIOD);
-			flag_write <= '1';
-			for i in mim_value to max_value loop
-				wait for PERIOD;
-		   end loop;
-         flag_write <= '0';			
-		 wait;
-end process gera_estimulos_saida_process;   
-	
-	
-	
-	-- escreve relatorio
-escreve_relatorio: process
-	variable linha : line;
-	variable output : std_logic_vector(3 downto 0); 
-	begin
-		--wait until clk ='0';
-		while true loop
-			if (flag_write ='1')then
-				output := vacinas_output;
-				write(linha, output);
-				writeline(refrigerador_1,linha);
-			 end if;
-			 wait for PERIOD;
-		 end loop; 
-end process escreve_relatorio;   
-	-- escrita do relatorio 
 	escrita_vacina_outputs:process
 		variable linha  : line;
 		variable saida : integer;
@@ -162,7 +154,10 @@ end process escreve_relatorio;
 	begin
 		wait until (falling_edge(clk));
 		while true loop
-			saida := mensagem(1);
+			write(linha,numeroRefrigerador);
+			write(linha,espaco);
+			
+			saida := notificacao;
 			if(saida = 1) then
 				write(linha, mensagem1);
 			elsif(saida = 2) then
@@ -178,6 +173,8 @@ end process escreve_relatorio;
 			end if;
 			
 				write(linha,espaco);
+				write(linha,loteVacina);
+				write(linha,espaco);
 				write(linha,temperatura_1);
 				write(linha,espaco);
 				write(linha,porta_1);
@@ -187,12 +184,9 @@ end process escreve_relatorio;
 		end loop; 
 	end process escrita_vacina_outputs; 
 	
-	
-	
 		
 		
-		instancia_vacina: vacina port map (temp_1=>temperatura_1, sensor_1 => porta_1);
-		temperatura_1 <= "0000000000000100", "0000000000000010" after 10 ns, "0000000000000011" after 20 ns, "0000000000000111" after 30 ns, "0000000000000010" after 40ns, "0000000000000010" after 50ns, "0000000000000001" after 60ns;
-		porta_1 <= false, true after 15 ns, false after 5 ns, true after 30 ns, false after 20 ns, true after 50 ns;
+		--temperatura_1 <= "0000000000000100", "0000000000000010" after 10 ns, "0000000000000011" after 20 ns, "0000000000000111" after 30 ns, "0000000000000010" after 40ns, "0000000000000010" after 50ns, "0000000000000001" after 60ns;
+		--porta_1 <= false, true after 15 ns, false after 5 ns, true after 30 ns, false after 20 ns, true after 50 ns;
 	
 end tb_gestao_vacinas;
